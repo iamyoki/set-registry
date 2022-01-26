e7d57fefa7c399131f526b0eb7603027e1dfa5e7
Yoki Yu <yokiyuqian@gmail.com>
2022-01-26 15:54:40 +0800

    chore: config web script, edit app component

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,25 +1,7 @@
 import React from 'react';
 
-import './App.css';
-
 function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          rel='noopener noreferrer'
-          target='_blank'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className='App'>header</div>;
 }
 
 export default App;
