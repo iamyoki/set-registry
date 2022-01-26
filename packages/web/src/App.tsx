8c788557695a12be4185f75468ad92522b333cd3
Yoki Yu <yokiyuqian@gmail.com>
2022-01-26 12:17:57 +0800

    refactor: edit configs

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img alt='logo' className='App-logo' src={logo} />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
